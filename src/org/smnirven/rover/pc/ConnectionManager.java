4b32b44c24d31cd59305e795f396afc3427afcb8
Thomas Steffes <smnirven@gmail.com>
2011-03-22 23:03:57 -0400

    latest rover command stuff

--- src/org/smnirven/rover/pc/ConnectionManager.java
+++ src/org/smnirven/rover/pc/ConnectionManager.java
@@ -23,84 +23,84 @@ import lejos.pc.comm.NXTInfo;
  */
 public class ConnectionManager {
 
-    private static ConnectionManager _instance;
-    private static NXTComm _connection;
-    private static DataOutputStream _dos;
-    private static DataInputStream _dis;
-    private static int[] _reply = new int[1000];
+  private static ConnectionManager _instance;
+  private static NXTComm _connection;
+  private static DataOutputStream _dos;
+  private static DataInputStream _dis;
+  private static int[] _reply = new int[1000];
 
-    private ConnectionManager() {
-        try {
-            _connection = NXTCommFactory.createNXTComm(NXTCommFactory.BLUETOOTH);
-        } catch (NXTCommException ex) {
-            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ex);
-        }
+  private ConnectionManager() {
+    try {
+      _connection = NXTCommFactory.createNXTComm(NXTCommFactory.BLUETOOTH);
+    } catch (NXTCommException ex) {
+      Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+  public static ConnectionManager getInstance() {
+    if (_instance == null) {
+      _instance = new ConnectionManager();
     }
 
-    public static ConnectionManager getInstance() {
-        if (_instance == null) {
-            _instance = new ConnectionManager();
-        }
+    return _instance;
+  }
 
-        return _instance;
+  public boolean openConnection(NxtDevice device) {
+    boolean itWorked = false;
+    try {
+      itWorked = _connection.open(device.getNxtInfo());
+      InputStream is = _connection.getInputStream();
+      OutputStream os = _connection.getOutputStream();
+
+      _dos = new DataOutputStream(os);
+      _dis = new DataInputStream(is);
+    } catch (NXTCommException ex) {
+      Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    return itWorked;
+  }
+
+  public int[] sendCommand(int[] command) {
+    if (_dos == null) {
+      System.out.println("dos is null");
     }
 
-    public boolean openConnection(NxtDevice device) {
-        boolean itWorked = false;
-        try {
-            itWorked = _connection.open(device.getNxtInfo());
-            InputStream is = _connection.getInputStream();
-            OutputStream os = _connection.getOutputStream();
-
-            _dos = new DataOutputStream(os);
-            _dis = new DataInputStream(is);
-        } catch (NXTCommException ex) {
-            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return itWorked;
+    for (int k = 0; k < 3; k++) {
+      try {
+        _dos.writeInt(command[k]);
+        _dos.flush();
+      } catch (IOException ioe) {
+        Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ioe);
+      }
     }
 
-    public int[] sendCommand(int[] command) {
-        if (_dos == null) {
-            System.out.println("dos is null");
-        }
+//    for (int k = 0; k < 8; k++) {
+//      try {
+//        _reply[k] = _dis.readInt();
+//      } catch (IOException ioe) {
+//        Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ioe);
+//      }
+//    }
 
-        for (int k = 0; k < 3; k++) {
-            try {
-                _dos.writeInt(command[k]);
-                _dos.flush();
-            } catch (IOException ioe) {
-                Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ioe);
-            }
-        }
+    return _reply;
+  }
 
-        for (int k = 0; k < 8; k++) {
-            try {
-                _reply[k] = _dis.readInt();
-            } catch (IOException ioe) {
-                Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ioe);
-            }
-        }
+  public ArrayList<NxtDevice> getAvailableDevices() {
+    ArrayList<NxtDevice> devices = new ArrayList();
 
-        return _reply;
+    try {
+      NXTInfo[] nxtInfos = _connection.search(null, NXTCommFactory.BLUETOOTH);
+      for (int i = 0; i < nxtInfos.length; i++) {
+        devices.add(new NxtDevice(nxtInfos[i]));
+      }
+    } catch (NXTCommException ex) {
+      Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ex);
     }
 
-    public ArrayList<NxtDevice> getAvailableDevices() {
-        ArrayList<NxtDevice> devices = new ArrayList();
+    return devices;
+  }
 
-        try {
-            NXTInfo[] nxtInfos = _connection.search(null, NXTCommFactory.BLUETOOTH);
-            for (int i = 0; i < nxtInfos.length; i++) {
-                devices.add(new NxtDevice(nxtInfos[i]));
-            }
-        } catch (NXTCommException ex) {
-            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        return devices;
-    }
-
-    public int[] getReply() {
-        return _reply;
-    }
+  public int[] getReply() {
+    return _reply;
+  }
 }
--- src/org/smnirven/rover/pc/RoverManager.java
+++ src/org/smnirven/rover/pc/RoverManager.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.smnirven.rover.pc;
 
 /**
@@ -10,14 +9,20 @@ package org.smnirven.rover.pc;
  * @author smnirven
  */
 public class RoverManager {
-    public static final int[] COMMAND_BATTERY_VOLTAGE = {0,0,0};
-    public static final int[] COMMAND_RED_LIGHT_ON = {1,0,0};
-    
-    public static void sendRedLight() {
-        ConnectionManager.getInstance().sendCommand(COMMAND_RED_LIGHT_ON);
-    }
 
-    public static int getBatteryLevel() {
-        return ConnectionManager.getInstance().sendCommand(COMMAND_RED_LIGHT_ON)[0];
-    }
+  public static final int[] COMMAND_BATTERY_VOLTAGE = {0, 0, 0};
+  public static final int[] COMMAND_ALL_STOP = {1, 0, 0};
+  public static final int[] COMMAND_RED_LIGHT_ON = {2, 0, 0};
+
+  public static void sendRedLight() {
+    ConnectionManager.getInstance().sendCommand(COMMAND_RED_LIGHT_ON);
+  }
+
+  public static void sendAllStop() {
+    ConnectionManager.getInstance().sendCommand(COMMAND_ALL_STOP);
+  }
+
+  public static int getBatteryLevel() {
+    return ConnectionManager.getInstance().sendCommand(COMMAND_RED_LIGHT_ON)[0];
+  }
 }
